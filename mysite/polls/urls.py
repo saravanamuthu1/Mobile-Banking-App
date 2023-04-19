2572111ecb79a13316e558ad8b2c82881875bdb3
smuthusamy <saravanamuthu.muthusamy@slu.edu>
2023-04-18 22:25:50 -0500

    added request money view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    #path("", views.formview, name="index"),
     path("", views.search_view, name='search_view'),
-    path("exchange/",views.exchange_view,name='exchange_view')
+    path("exchange/",views.exchange_view,name='exchange_view'),
+    path("stocks/",views.stock_view,name="stock_view"),
+    path("request/",views.request_view,name="request_view"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,7 +4,11 @@ from .forms import NameForm
 import requests
 import json
 import time
-from .models import apidata
+from .models import apidata,money
+from datetime import date
+from django.core.mail import send_mail,BadHeaderError
+from django.conf import settings
+from django.contrib import messages
 # Create your views here.
 
 
@@ -37,3 +41,31 @@ def exchange_view(request):
         'currency_to':currency_to    
     }
     return render(request,'polls/base.html', context)
+def stock_view(request):
+    today=date.today()
+    balance_amount=money.objects.all()
+    print(balance_amount)
+    context={
+        'date':today,
+        'balance_amount':balance_amount[0],
+    }
+    print(balance_amount[0])
+    return render(request,'polls/Pay.html',context)
+def request_view(request):
+    email=''
+    message=""
+    title="request money"
+    if request.method == 'POST':
+        email=request.POST.get("email")
+        message=request.POST.get("message")
+        try: 
+            send_mail(
+                title,
+                message,
+                settings.EMAIL_HOST_USER,
+                [email]
+            )
+            return HttpResponseRedirect("/stocks/")
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+    return render(request,'polls/requestmoney.html')
